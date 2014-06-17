2f004c659246b952331ad334b39a73a35cf84f16
jdoming3@nd.edu <jdoming3@nd.edu@d55e335f-fc90-06e6-7c11-ac3f77601ff6>
2014-06-17 10:15:43 +0000

    Created initial version of the LandUse class and its associated registry for looking up land uses by their map codes.  The LandUse class has all the required properties except the land cover change.

---
+++ exts/land-use/trunk/src/LandUse.cs
@@ -0,0 +1,31 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.SpatialModeling;
+
+namespace Landis.Extension.LandUse
+{
+    /// <summary>
+    /// The definition of a particular land use.
+    /// </summary>
+    public class LandUse
+    {
+        public string Name { get; protected set; }
+        public ushort MapCode { get; protected set; }
+        public bool AllowHarvest { get; protected set; }
+
+        //---------------------------------------------------------------------
+
+        public LandUse(
+            string name,
+            ushort mapCode,
+            bool harvestingAllowed)
+        {
+            Name = name;
+            MapCode = mapCode;
+            AllowHarvest = harvestingAllowed;
+        }
+    }
+}
