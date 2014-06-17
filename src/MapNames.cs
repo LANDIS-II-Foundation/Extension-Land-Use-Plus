2079614cd7464c63e81e1a656e5d8857d1f73143
Jimm Domingo <jimm-domingo@users.noreply.github.com>
2014-06-17 10:57:59 +0000

    Copied the class from Base Harvest 1.3 for handling paths to maps.

---
+++ src/MapNames.cs
@@ -0,0 +1,43 @@
+using Edu.Wisc.Forest.Flel.Util;
+using System.Collections.Generic;
+
+namespace Landis.Harvest
+{
+    /// <summary>
+    /// Methods for working with the template for the names of prescription
+    /// maps.
+    /// </summary>
+    public static class MapNames
+    {
+        public const string TimestepVar = "timestep";
+
+        private static IDictionary<string, bool> knownVars;
+        private static IDictionary<string, string> varValues;
+
+        //---------------------------------------------------------------------
+
+        static MapNames()
+        {
+            knownVars = new Dictionary<string, bool>();
+            knownVars[TimestepVar] = true;
+
+            varValues = new Dictionary<string, string>();
+        }
+
+        //---------------------------------------------------------------------
+
+        public static void CheckTemplateVars(string template)
+        {
+            OutputPath.CheckTemplateVars(template, knownVars);
+        }
+
+        //---------------------------------------------------------------------
+
+        public static string ReplaceTemplateVars(string template,
+                                                 int    timestep)
+        {
+            varValues[TimestepVar] = timestep.ToString();
+            return OutputPath.ReplaceTemplateVars(template, varValues);
+        }
+    }
+}
