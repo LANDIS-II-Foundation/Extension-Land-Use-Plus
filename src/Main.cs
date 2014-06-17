3f66f32c65f281b5788ca32f003603b9f0880aca
Jimm Domingo <jimm-domingo@users.noreply.github.com>
2014-06-17 09:58:42 +0000

    Stubbed out the extension's main class and parameter parser.  The parser only supports the extension's Timestep parameter.  Also added the extension's info file.  Used the info file to register the extension, and then confirmed the model can load and run the extension.

---
+++ src/Main.cs
@@ -0,0 +1,52 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Core;
+
+namespace Landis.Extension.LandUse
+{
+    public class Main
+        : Landis.Core.ExtensionMain
+    {
+        public static readonly ExtensionType ExtType = new ExtensionType("disturbance:land use");
+        public static readonly string ExtensionName = "Land Use";
+
+        private static ICore modelCore;
+        private Parameters parameters;
+
+        //---------------------------------------------------------------------
+
+        public Main()
+            : base(ExtensionName, ExtType)
+        {
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void LoadParameters(string dataFile,
+                                            ICore mCore)
+        {
+            modelCore = mCore;
+            modelCore.UI.WriteLine("  Loading parameters from {0}", dataFile);
+            ParameterParser parser = new ParameterParser();
+            parameters = Landis.Data.Load<Parameters>(dataFile, parser);
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void Initialize()
+        {
+            modelCore.UI.WriteLine("Initializing {0}...", Name);
+            Timestep = parameters.Timestep;
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void Run()
+        {
+            modelCore.UI.WriteLine("  (in Run method)");
+        }
+    }
+}
