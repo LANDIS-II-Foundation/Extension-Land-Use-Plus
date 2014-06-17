f4a3b94025aa25e087511c0ca44876e6fc31ebde
Jimm Domingo <jimm-domingo@users.noreply.github.com>
2014-06-17 10:15:43 +0000

    Created initial version of the LandUse class and its associated registry for looking up land uses by their map codes.  The LandUse class has all the required properties except the land cover change.

--- src/ParameterParser.cs
+++ src/ParameterParser.cs
@@ -5,6 +5,7 @@
 
 using Edu.Wisc.Forest.Flel.Util;
 using Landis.Core;
+using System.Collections.Generic;
 
 namespace Landis.Extension.LandUse
 {
@@ -43,7 +44,51 @@ namespace Landis.Extension.LandUse
             ReadVar(timestep);
             parameters.Timestep = timestep.Value;
 
+            ReadLandUses();
             return parameters;
+        }
+
+        //---------------------------------------------------------------------
+
+        protected void ReadLandUses()
+        {
+            InputVar<string> name = new InputVar<string>("LandUse");
+            InputVar<ushort> mapCode = new InputVar<ushort>("MapCode");
+            InputVar<bool> allowHarvest = new InputVar<bool>("AllowHarvest?");
+
+            Dictionary<string, int> nameLineNumbers = new Dictionary<string, int>();
+            Dictionary<ushort, int> mapCodeLineNumbers = new Dictionary<ushort, int>();
+
+            while (!AtEndOfInput)
+            {
+                int nameLineNum = LineNumber;
+                ReadVar(name);
+                int lineNumber;
+                if (nameLineNumbers.TryGetValue(name.Value.Actual, out lineNumber))
+                    throw new InputValueException(name.Value.String,
+                                                  "The land use \"{0}\" was previously used on line {1}",
+                                                  name.Value.Actual, lineNumber);
+                else
+                {
+                    nameLineNumbers[name.Value.Actual] = nameLineNum;
+                }
+
+                int mapCodeLineNum = LineNumber;
+                ReadVar(mapCode);
+                if (mapCodeLineNumbers.TryGetValue(mapCode.Value.Actual, out lineNumber))
+                    throw new InputValueException(mapCode.Value.String,
+                                                  "The map code \"{0}\" was previously used on line {1}",
+                                                  mapCode.Value.Actual, lineNumber);
+                else
+                    mapCodeLineNumbers[mapCode.Value.Actual] = mapCodeLineNum;
+
+                ReadVar(allowHarvest);
+
+                LandUse landUse = new LandUse(name.Value.Actual,
+                                              mapCode.Value.Actual,
+                                              allowHarvest.Value.Actual);
+                LandUseRegistry.Register(landUse);
+            }
         }
     }
 }
