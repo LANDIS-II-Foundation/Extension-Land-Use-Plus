0828700df3accccec822a52790efd63e4b120aae
jdoming3@nd.edu <jdoming3@nd.edu@d55e335f-fc90-06e6-7c11-ac3f77601ff6>
2014-07-07 17:37:51 +0000

    Tagged version 0.1

---
+++ exts/land-use/tags/0.1/src/LandCover/RemoveTreesWithPartialHarvest.cs
@@ -0,0 +1,54 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Library.BiomassCohorts;
+using Landis.Library.BiomassHarvest;
+using Landis.SpatialModeling;
+
+namespace Landis.Extension.LandUse.LandCover
+{
+    /// <summary>
+    /// A land cover change that removes trees with partial thinning (i.e.,
+    /// a percentage of one or more cohorts are harvested).
+    /// </summary>
+    public class RemoveTreesWithPartialHarvest
+        : BiomassCohortHarvest, IChange
+    {
+        public const string TypeName = "RemoveTrees";
+
+        //---------------------------------------------------------------------
+
+        string IChange.Type
+        {
+            get { return TypeName; }
+        }
+
+        //---------------------------------------------------------------------
+
+        public RemoveTreesWithPartialHarvest(Landis.Library.Harvest.ICohortSelector cohortSelector,
+                                             PartialCohortSelectors                 partialCohortSelectors)
+            : base(cohortSelector, partialCohortSelectors)
+        {
+            base.Type = Main.ExtType;
+        }
+
+        //---------------------------------------------------------------------
+
+        public void ApplyTo(ActiveSite site)
+        {
+            CurrentSite = site;
+            Cut(site);
+        }
+
+        //---------------------------------------------------------------------
+
+        protected override void Record(int     reduction,
+                                       ICohort cohort)
+        {
+            if (SiteLog.Enabled)
+                SiteLog.RecordHarvest(cohort.Species, reduction);
+        }
+    }
+}
---
+++ exts/land-use/tags/0.1/src/Main.cs
@@ -0,0 +1,145 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Core;
+using Landis.SpatialModeling;
+using System.Collections.Generic;
+
+namespace Landis.Extension.LandUse
+{
+    public class Main
+        : Landis.Core.ExtensionMain
+    {
+        public static readonly ExtensionType ExtType = new ExtensionType("disturbance:land use");
+        public static readonly string ExtensionName = "Land Use";
+
+        private Parameters parameters;
+        private string inputMapTemplate;
+
+        //---------------------------------------------------------------------
+
+        public Main()
+            : base(ExtensionName, ExtType)
+        {
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void LoadParameters(string dataFile,
+                                            ICore modelCore)
+        {
+            Model.Core = modelCore;
+            Landis.Library.BiomassHarvest.Main.InitializeLib(Model.Core);
+            Model.Core.UI.WriteLine("  Loading parameters from {0}", dataFile);
+            ParameterParser parser = new ParameterParser(Model.Core.Species);
+            parameters = Landis.Data.Load<Parameters>(dataFile, parser);
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void Initialize()
+        {
+            Model.Core.UI.WriteLine("Initializing {0}...", Name);
+            SiteVars.Initialize(Model.Core);
+            Timestep = parameters.Timestep;
+            inputMapTemplate = parameters.InputMaps;
+            if (parameters.SiteLogPath != null)
+                SiteLog.Initialize(parameters.SiteLogPath);
+
+            // Load initial land uses from input map for timestep 0
+            ProcessInputMap(
+                delegate(Site site,
+                         LandUse initialLandUse)
+                {
+                    SiteVars.LandUse[site] = initialLandUse;
+                    return initialLandUse.Name;
+                });
+        }
+
+        //---------------------------------------------------------------------
+
+        public override void Run()
+        {
+            if (SiteLog.Enabled)
+                SiteLog.TimestepSetUp();
+
+            ProcessInputMap(
+                delegate(Site site,
+                         LandUse newLandUse)
+                {
+                    LandUse currentLandUse = SiteVars.LandUse[site];
+                    if (newLandUse != currentLandUse)
+                    {
+                        SiteVars.LandUse[site] = newLandUse;
+                        newLandUse.LandCoverChange.ApplyTo((ActiveSite)site);
+                        if (SiteLog.Enabled)
+                            SiteLog.WriteTotalsFor((ActiveSite)site);
+
+                        string transition = string.Format("{0} --> {1}", currentLandUse.Name, newLandUse.Name);
+                        return transition;
+                    }
+                    else
+                        return null;
+                });
+
+            if (SiteLog.Enabled)
+                SiteLog.TimestepTearDown();
+        }
+
+        //---------------------------------------------------------------------
+
+        // A delegate for processing a land use read from an input map.
+        public delegate string ProcessLandUseAt(Site site, LandUse landUse);
+
+        //---------------------------------------------------------------------
+
+        public void ProcessInputMap(ProcessLandUseAt processLandUseAt)
+        {
+            string inputMapPath = MapNames.ReplaceTemplateVars(inputMapTemplate, Model.Core.CurrentTime);
+            Model.Core.UI.WriteLine("  Reading map \"{0}\"...", inputMapPath);
+            IInputRaster<MapPixel> inputMap;
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            using (inputMap = Model.Core.OpenRaster<MapPixel>(inputMapPath))
+            {
+                MapPixel pixel = inputMap.BufferPixel;
+                foreach (Site site in Model.Core.Landscape)
+                {
+                    inputMap.ReadBufferPixel();
+                    if (site.IsActive)
+                    {
+                        LandUse landUse = LandUseRegistry.LookUp(pixel.LandUseCode.Value);
+                        if (landUse == null)
+                        {
+                            string message = string.Format("Error: Unknown map code ({0}) at pixel {1}",
+                                                           pixel.LandUseCode.Value,
+                                                           site.Location);
+                            throw new System.ApplicationException(message);
+                        }
+                        string key = processLandUseAt(site, landUse);
+                        if (key != null)
+                        {
+                            int count;
+                            if (counts.TryGetValue(key, out count))
+                                count = count + 1;
+                            else
+                                count = 1;
+                            counts[key] = count;
+                        }
+                    }
+                }
+            }
+            foreach (string key in counts.Keys)
+                Model.Core.UI.WriteLine("    {0} ({1:#,##0})", key, counts[key]);
+        }
+
+        //---------------------------------------------------------------------
+
+        public new void CleanUp()
+        {
+            if (SiteLog.Enabled)
+                SiteLog.Close();
+        }
+    }
+}
