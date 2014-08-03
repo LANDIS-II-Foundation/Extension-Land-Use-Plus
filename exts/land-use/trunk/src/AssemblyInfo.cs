a17936a73eb1f92bc0ce8b265f42e719908eae44
jdoming3@nd.edu <jdoming3@nd.edu@d55e335f-fc90-06e6-7c11-ac3f77601ff6>
2014-08-03 21:32:18 +0000

    Initiated development of version 0.4

--- exts/land-use/trunk/src/AssemblyInfo.cs
+++ exts/land-use/trunk/src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Land Use Extension")]
-[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyVersion("0.4.*")]
 [assembly: AssemblyDescription("Land Use Extension for LANDIS-II")]
 [assembly: AssemblyCopyright("2014 University of Notre Dame")]
 
