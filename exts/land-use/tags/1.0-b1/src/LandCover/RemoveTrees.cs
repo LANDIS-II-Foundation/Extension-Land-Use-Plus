04b2797225eb01aa1c7c323a84a4ca5f8f4c3e2e
jdoming3@nd.edu <jdoming3@nd.edu@d55e335f-fc90-06e6-7c11-ac3f77601ff6>
2014-12-02 22:08:22 +0000

    Tagged the beta 1 release of version 1.0

---
+++ exts/land-use/tags/1.0-b1/src/LandCover/RemoveTrees.cs
@@ -0,0 +1,54 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Library.SiteHarvest;
+using Landis.Library.Succession;
+using Landis.SpatialModeling;
+using log4net;
+
+namespace Landis.Extension.LandUse.LandCover
+{
+    class RemoveTrees
+        : IChange
+    {
+        public const string TypeName = "RemoveTrees";
+        private ICohortCutter cohortCutter;
+        private Planting.SpeciesList speciesToPlant;
+        private static readonly ILog log = LogManager.GetLogger(typeof(RemoveTrees));
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+
+        //---------------------------------------------------------------------
+
+        string IChange.Type
+        {
+            get { return TypeName; }
+        }
+
+        //---------------------------------------------------------------------
+
+        public RemoveTrees(ICohortCutter        cohortCutter,
+                           Planting.SpeciesList speciesToPlant)
+        {
+            this.cohortCutter = cohortCutter;
+            this.speciesToPlant = speciesToPlant;
+        }
+
+        //---------------------------------------------------------------------
+
+        public void ApplyTo(ActiveSite site)
+        {
+            if (isDebugEnabled)
+                log.DebugFormat("    Applying LCC {0} to site {1}",
+                                GetType().Name,
+                                site.Location);
+
+            // For now, we don't do anything with the counts of cohorts cut.
+            CohortCounts cohortCounts = new CohortCounts();
+            cohortCutter.Cut(site, cohortCounts);
+            if (speciesToPlant != null)
+                Reproduction.ScheduleForPlanting(speciesToPlant, site);
+        }
+    }
+}
---
+++ exts/land-use/tags/1.0-b1/src/SiteLog.cs
@@ -0,0 +1,88 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Core;
+using Landis.Library.BiomassCohorts;
+using Landis.Library.BiomassHarvest;
+using Landis.SpatialModeling;
+using log4net;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Landis.Extension.LandUse
+{
+    /// <summary>
+    /// A log file with details about the biomass removed at each site.
+    /// </summary>
+    public static class SiteLog
+    {
+        public static bool Enabled { get; private set; }
+        private static StreamWriter logFile;
+        private static readonly ILog log = LogManager.GetLogger(typeof(SiteLog));
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+
+        //---------------------------------------------------------------------
+
+        static SiteLog()
+        {
+            Enabled = false;
+        }
+
+        //---------------------------------------------------------------------
+
+        public static void Initialize(string path)
+        {
+            Model.Core.UI.WriteLine("  Opening log file \"{0}\"...", path);
+            logFile = Landis.Data.CreateTextFile(path);
+            logFile.Write("timestep,row,column");
+            foreach (ISpecies species in Model.Core.Species)
+                logFile.Write(",{0}", species.Name);
+            logFile.WriteLine();
+            Enabled = true;
+
+            SiteBiomass.ResetHarvestTotals();
+        }
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Prepare the log file for the extension's execution during current
+        /// timestep.
+        /// </summary>
+        public static void TimestepSetUp()
+        {
+            SiteBiomass.EnableRecordingForHarvest();
+        }
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Clean up at the end of the extension's execution during the current
+        /// timestep.
+        /// </summary>
+        public static void TimestepTearDown()
+        {
+            SiteBiomass.DisableRecordingForHarvest();
+        }
+
+        //---------------------------------------------------------------------
+
+        public static void WriteTotalsFor(ActiveSite site)
+        {
+            logFile.Write("{0},{1},{2}", Model.Core.CurrentTime, site.Location.Row, site.Location.Column);
+            foreach (ISpecies species in Model.Core.Species)
+                logFile.Write(",{0}", SiteBiomass.Harvested[species]);
+            logFile.WriteLine();
+            SiteBiomass.ResetHarvestTotals();
+        }
+
+        //---------------------------------------------------------------------
+
+        public static void Close()
+        {
+            logFile.Close();
+        }
+    }
+}
