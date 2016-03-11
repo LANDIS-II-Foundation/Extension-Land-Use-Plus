d2eae679906ca08ada742ca9e4595809f500d057
brmiranda <brmiranda@fs.fed.us>
2016-03-11 08:19:41 -0600

    Update to version 1.1, including User Guide.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Land Use Extension")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyDescription("Land Use Extension for LANDIS-II")]
 [assembly: AssemblyCopyright("2014 University of Notre Dame")]
 
