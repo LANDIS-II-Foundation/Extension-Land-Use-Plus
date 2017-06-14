18c70353fcfa41de3f4384fcd1507835ea5feea1
Adam Chmurzynski <Adam Chmurzynski>
2017-06-14 08:59:35 -0700

    Added insect defoliation functionality, Insect extension now build dependency

---
+++ src/InsectDefoliant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Landis.Core;
+using Landis.SpatialModeling;
+using Landis.Extension.Insects;
+
+namespace Landis.Extension.LandUse
+{
+     /**
+      * A class demonstrating LU+ connectivity to succession modules
+      * and Insect extension. Can directly call static Defoliation 
+      * methods from Insect library
+      **/
+    class InsectDefoliant
+    {
+        public InsectDefoliant()
+        {
+
+        }
+
+        public void InsectDefoliate(ActiveSite active, ISpecies species, int cohortBiomass, int siteBiomass)
+        {
+            //Do LU+ specific things?
+            Defoliate.DefoliateCohort(active, species, cohortBiomass, siteBiomass);
+        }
+    }
+}
